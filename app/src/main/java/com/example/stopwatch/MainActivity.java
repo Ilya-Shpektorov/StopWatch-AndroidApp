3e7dc663b09e04dbf8b312e33fbdd3b8594e1748
Ilya <medwayll@mail.ru>
2020-05-12 23:06:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTexrVieiw;
+    boolean isRunning = false;
+    private int seconds = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerTexrVieiw = findViewById(R.id.textViewTimer);
+        runTimer();
+    }
+
+    public void onClickStartTimer(View view) {
+        isRunning = true;
+    }
+
+    public void onClickPauseTimer(View view) {
+        isRunning = false;
+    }
+
+    public void onClickResetTimer(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(() -> {
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            int sec = seconds % 60;
+
+            String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, sec);
+            timerTexrVieiw.setText(time);
+            while (isRunning) {
+                seconds++;
+            }
+            handler.postDelayed(this::runTimer, 1000);
+        });
+    }
+}
